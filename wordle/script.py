8b4c1224839df845a9e11822159b864c41187f37
Eugene Petrov <eugene.petrov@essenceglobal.com>
2024-09-22 23:47:33 +0100

    working on wordle printing output

--- wordle/script.py
+++ wordle/script.py
@@ -1,35 +1,41 @@
 ## https://realpython.com/python-wordle-clone/
 
 import random
+from rich.console import Console
+from rich.theme import Theme
 
-MAX_GUESSES = 6     #a variable to determine the number of guesses
+console = Console()
+
+MAX_GUESSES = 6     #set a variable to determine the number of guesses
 
 def main():
     # Pre-process
     secret_word = get_random_word('word_list.txt')
-
+    user_guesses = ["_" * 5] * MAX_GUESSES
+    
     # Process (main loop)
     for guess_num in range(0, MAX_GUESSES):     #start a loop for the number of guesses
+        
+        refresh_screen(f'Guess #{guess_num + 1}')
+        show_guesses(user_guesses, secret_word)
 
-        guess = input(f'\nGuess {guess_num + 1}: ').upper()
+        user_guesses[guess_num] = input('\nYour Guess: ').upper()
         while True:
-            if len(guess) != 5:
-                guess = input('The guess should be a 5 letter word! \nTry again: ').upper()
+            if len(user_guesses[guess_num]) != 5:
+                user_guesses[guess_num] = input('The guess should be a 5 letter word! \nTry again: ').upper()
             else: 
                 break
 
-        show_guess(guess, secret_word)
-        if guess == secret_word:
+        if user_guesses[guess_num] == secret_word:
             break
 
     # Post-process
     else:
+        show_guesses(user_guesses, secret_word)
         game_over(secret_word)
 
 def get_random_word(filename):
-
     word_list = []      #create an empty list for storing words
-
     #read a file and append into the list
     with open(filename, 'r') as file:
         word_list = [
@@ -37,11 +43,9 @@ def get_random_word(filename):
             for word in file 
             if len(word.replace('\n', '')) == 5
             ]
-    
     return random.choice(word_list)      #randomly select a word from the list
 
 def show_guess(guess, secret_word):
-
     correct_letters = {letter for letter, correct in zip(guess, secret_word) if letter == correct}
     misplaced_letters = set(guess) & set(secret_word) - correct_letters
     wrong_letters = set(guess) - set(secret_word)
@@ -49,6 +53,23 @@ def show_guess(guess, secret_word):
     print('Correct letters:', ', '.join(sorted(correct_letters)))
     print('Misplaced letters:', ', '.join(sorted(misplaced_letters)))
     print('Wrong letters:', ', '.join(sorted(wrong_letters)))
+
+def show_guesses(guesses, secret_word):
+    for guess in guesses:
+        styled_guess = []
+        for letter, correct in zip(guess, secret_word):
+            if letter == correct:
+                style = 'bold white on green'
+            elif letter in secret_word:
+                style = 'bold white on yellow"'
+            else: 
+                style = 'white on #666666' 
+            styled_guess.append(f'[{style}]{letter}[/]')
+        console.print(''.join(styled_guess))
+
+def refresh_screen(headline):
+    console.clear()
+    console.rule(f"[bold blue]:leafy_green: {headline} :leafy_green:[/]\n")
 
 def game_over(secret_word):
     print(f'The word was {secret_word}')
